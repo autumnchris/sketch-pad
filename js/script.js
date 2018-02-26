8c9845fc0d63883fa4ae1cc90da3147a77a9e58e
Autumn <autumncbullard@gmail.com>
2018-02-26 15:41:03 -0500

    Build function that will create a grid

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function createGrid() {
+  var numOfSquares = document.getElementById('size-input').value;
+  var colSize = 400 / numOfSquares;
+  colSize = Math.round(colSize);
+
+  for (var i = 0; i < numOfSquares; i++) {
+    var row = document.createElement('div');
+    row.className = 'row';
+    document.getElementById('grid').appendChild(row);
+  }
+
+  document.querySelectorAll('.row').forEach(function(rows) {
+
+    for (var i = 0; i < numOfSquares; i++) {
+      var col = document.createElement('div');
+      col.className = 'col';
+      rows.appendChild(col);
+    }
+  });
+
+  document.querySelectorAll('.col').forEach(function(colStyle) {
+    colStyle.setAttribute('style', 'height: ' + colSize + 'px; width: ' + colSize + 'px;');
+  });
+}
+
+createGrid();
