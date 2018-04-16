7be4abee15461027283c994112bc358d743340b3
Autumn <autumncbullard@gmail.com>
2018-04-16 17:05:48 -0400

    Switch to ES6 syntax

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function createGrid() {
-  var row,
-  col,
-  i,
-  numOfSquares = document.getElementById('size-input').value,
-  colSize = Math.round(400 / numOfSquares);
+  let row;
+  let col;
+  let i;
+  const numOfSquares = document.getElementById('size-input').value;
+  const colSize = Math.round(400 / numOfSquares);
 
   for (i = 0; i < numOfSquares; i++) {
     row = document.createElement('div');
@@ -11,7 +11,7 @@ function createGrid() {
     document.querySelector('.grid').appendChild(row);
   }
 
-  document.querySelectorAll('.row').forEach(function(rows) {
+  document.querySelectorAll('.row').forEach(rows => {
 
     for (i = 0; i < numOfSquares; i++) {
       col = document.createElement('div');
@@ -20,30 +20,30 @@ function createGrid() {
     }
   });
 
-  document.querySelectorAll('.col').forEach(function(colStyle) {
-    colStyle.setAttribute('style', 'height: ' + colSize + 'px; width: ' + colSize + 'px;');
+  document.querySelectorAll('.col').forEach(colStyle => {
+    colStyle.setAttribute('style', `height: ${colSize}px; width: ${colSize}px;`);
 
-    colStyle.addEventListener('mouseenter', function(event) {
+    colStyle.addEventListener('mouseenter', event => {
       event.target.className = 'col shaded-col';
     });
   });
 
-  document.querySelector('.grid-size').innerHTML = 'Current grid size is ' + numOfSquares + ' x ' + numOfSquares + '.';
+  document.querySelector('.grid-size').innerHTML = `Current grid size is ${numOfSquares} x ${numOfSquares}.`;
 }
 
 createGrid();
 
-document.querySelector('.clear').addEventListener('click', function() {
-  document.querySelectorAll('.col').forEach(function(colStyle) {
+document.querySelector('.clear').addEventListener('click', () => {
+  document.querySelectorAll('.col').forEach(colStyle => {
     colStyle.className = 'col';
   });
 });
 
-document.querySelector('.resize').addEventListener('click', function() {
+document.querySelector('.resize').addEventListener('click', () => {
   document.getElementById('modal').style.display = 'block';
 });
 
-document.querySelector('.save').addEventListener('click', function() {
+document.querySelector('.save').addEventListener('click', () => {
 
   if (document.getElementById('size-input').value.match(/^[0-9]+$/) && document.getElementById('size-input').value >= 4 && document.getElementById('size-input').value <= 60) {
     document.querySelector('.error-message').style.display = 'none';
@@ -56,11 +56,11 @@ document.querySelector('.save').addEventListener('click', function() {
   }
 });
 
-document.querySelector('.cancel').addEventListener('click', function() {
+document.querySelector('.cancel').addEventListener('click', () => {
   document.getElementById('modal').style.display = 'none';
 });
 
-window.addEventListener('click', function(event) {
+window.addEventListener('click', event => {
 
   if (event.target.id === 'modal') {
     document.getElementById('modal').style.display = 'none';
